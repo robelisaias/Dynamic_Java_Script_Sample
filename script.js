9e7a3db6f352be8901540879c0011bc0d4529456
Robel Isaias <robelisaias@gmail.com>
2026-04-06 20:30:30 -0400

    Added a separate json file and refactored the code to use it

--- script.js
+++ script.js
@@ -1,55 +1,15 @@
-
-
-const projects = [
-    {
-        title: "Portfolio Website",
-        description: "A personal portfolio website using HTML, CSS, and JS.",
-        category: "Web Design",
-        imageURL: "Card_Images/web-design.jpg",
-        link: "#"
-    },
-    {
-        title: "Portfolio App",
-        description: "A simple portfolio app to showcase projects.",
-        category: "App Development",
-        imageURL: "Card_Images/App-portfolio.jpg",
-        link: "#"
-    },
-    {
-        title: "Portfolio Photo",
-        description: "A collection of photos showcasing my work.",
-        category: "Photography",
-        imageURL: "Card_Images/Photo-portfolio.jpg",
-        link: "#"
-    },
-    {
-        title: "Portfolio Architecture",
-        description: "A portfolio of architectural designs.",
-        category: "Architecture",
-        imageURL: "Card_Images/Architecture-portfolio.jpg",
-        link: "#"
-    },
-    {
-        title: "Portfolio Travel",
-        description: "A collection of travel sites for your next adventure.",
-        category: "Travel",
-        imageURL: "Card_Images/Travel-design.jpg",
-        link: "#"
-    },
-    {
-        title: "Portfolio Sports",
-        description: "A collection of sport shows.",
-        category: "Sports",
-        imageURL: "Card_Images/Sports-events.jpg",
-        link: "#"
-    }
-];
-
-
 const container = document.getElementById("card-container");
+const searchInput = document.getElementById("search");
+let projects = [];
+
+
 
 function renderCards(projectList) {
     container.innerHTML = ""; 
+
+    if (projectList.length === 0) {
+        container.innerHTML = "<p style='text-align:center; color: gray;'>No projects found.</p>";
+    }
 
     projectList.forEach(project => {
         const card = document.createElement("article");
@@ -64,13 +24,28 @@ function renderCards(projectList) {
 
         container.appendChild(card);
     });
+
+    
 }
 
+async function loadProjects() {
+    try {
+        const response = await fetch("projects.json");
+        if(!response.ok){
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
 
-renderCards(projects);
+        projects= await response.json();
+        renderCards(projects);
+    }
+    catch(error){
+        console.error("Error fetching projects", error);
+        container.innerHTML= "<p style='color:red;'>Failed to load projects.</p>";
+    }
+}
 
+loadProjects();
 
-const searchInput = document.getElementById("search");
 
 searchInput.addEventListener("input", () => {
     const query = searchInput.value.toLowerCase();
